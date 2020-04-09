50c014b280b9272553ceb195229251f946567334
Jake Booher <jake.booher@gmail.com>
2020-04-09 15:05:52 -0500

    Migration Creator update to default timestampsTz()

--- src/Illuminate/Database/Migrations/MigrationCreator.php
+++ src/Illuminate/Database/Migrations/MigrationCreator.php
@@ -21,10 +21,12 @@ class MigrationCreator extends \Illuminate\Database\Migrations\MigrationCreator
             "<?php\n\n",
             'use Illuminate\Database\Schema\Blueprint;' . "\n",
             '$table->bigIncrements(\'id\');',
+            '$table->timestamps();',
         ], [
             "<?php\n\n" . implode("\n", $additionalClasses) . "\n",
             '',
             '$table->id();',
+            '$table->timestampsTz();',
         ], $stub);
 
         return $stub;
