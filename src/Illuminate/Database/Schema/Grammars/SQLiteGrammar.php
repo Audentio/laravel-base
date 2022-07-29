fd1f97bf6ebcb362ee1128c88ee385fba52c120b
Jake B <jake.booher@gmail.com>
2022-07-29 16:05:43 -0500

    Update SQLiteGrammar.php

--- src/Illuminate/Database/Schema/Grammars/SQLiteGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/SQLiteGrammar.php
@@ -9,13 +9,25 @@ class SQLiteGrammar extends \Illuminate\Database\Schema\Grammars\SQLiteGrammar
 {
     protected function modifyDefault(Blueprint $blueprint, Fluent $column)
     {
-        if (!$column->nullable && $column->default === null && $column->type === 'string') {
-            $column->default = '';
+        if (!$column->nullable && $column->default === null) {
+            if (in_array($column->type, [
+                'string', 'uuid',
+            ])) {
+                $column->default = '';
+            }
+            if ($column->type === 'enum') {
+                $column->default = $column->allowed[0] ?? '';
+            }
         }
 
         if (! is_null($column->default) && is_null($column->virtualAs) && is_null($column->storedAs)) {
             return ' default '.$this->getDefaultValue($column->default);
         }
+    }
+
+    protected function typeEnum(Fluent $column): string
+    {
+        return 'varchar';
     }
 
     protected function typeBlob(Fluent $column): string
