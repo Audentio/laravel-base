b0a9cea76aa119e8d258acc40b5821dfb8a530e5
Jake B <jake.booher@gmail.com>
2022-07-29 15:47:22 -0500

    Update SQLiteGrammar.php
    Set default value in SQLite to '' for non nullable strings.

--- src/Illuminate/Database/Schema/Grammars/SQLiteGrammar.php
+++ src/Illuminate/Database/Schema/Grammars/SQLiteGrammar.php
@@ -2,10 +2,22 @@
 
 namespace Audentio\LaravelBase\Illuminate\Database\Schema\Grammars;
 
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Fluent;
 
 class SQLiteGrammar extends \Illuminate\Database\Schema\Grammars\SQLiteGrammar
 {
+    protected function modifyDefault(Blueprint $blueprint, Fluent $column)
+    {
+        if (!$column->nullable && $column->default === null && $column->type === 'string') {
+            $column->default = '';
+        }
+
+        if (! is_null($column->default) && is_null($column->virtualAs) && is_null($column->storedAs)) {
+            return ' default '.$this->getDefaultValue($column->default);
+        }
+    }
+
     protected function typeBlob(Fluent $column): string
     {
         return 'blob';
